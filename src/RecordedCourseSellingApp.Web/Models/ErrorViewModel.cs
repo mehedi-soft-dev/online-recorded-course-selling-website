71264b2d3f8bd6ca9924b05c2b79bd3a3f9ff504
Md. Mehedi Hasan <cse.mdmehedi@gmail.com>
2023-04-20 00:29:16 +0600

    create web project

---
+++ src/RecordedCourseSellingApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RecordedCourseSellingApp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
