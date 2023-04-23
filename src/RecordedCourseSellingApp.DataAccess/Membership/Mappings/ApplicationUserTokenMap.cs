3c5a94ffccf127976380670a03748cb5be78c44e
Md. Mehedi Hasan <cse.mdmehedi@gmail.com>
2023-04-23 16:51:48 +0600

    add application user token entity map

---
+++ src/RecordedCourseSellingApp.DataAccess/Membership/Mappings/ApplicationUserTokenMap.cs
@@ -0,0 +1,37 @@
+using NHibernate;
+using NHibernate.Mapping.ByCode.Conformist;
+using RecordedCourseSellingApp.DataAccess.Membership.Entities;
+
+namespace RecordedCourseSellingApp.DataAccess.Membership.Mappings;
+
+public class ApplicationUserTokenMap : ClassMapping<ApplicationUserToken>
+{
+    public ApplicationUserTokenMap()
+    {
+        Schema("dbo");
+        Table("ApplicationUserTokens");
+        ComposedId(id => {
+            id.Property(e => e.UserId, prop => {
+                prop.Column("UserId");
+                prop.Type(NHibernateUtil.String);
+                prop.Length(32);
+            });
+            id.Property(e => e.LoginProvider, prop => {
+                prop.Column("LoginProvider");
+                prop.Type(NHibernateUtil.String);
+                prop.Length(32);
+            });
+            id.Property(e => e.Name, prop => {
+                prop.Column("Name");
+                prop.Type(NHibernateUtil.String);
+                prop.Length(32);
+            });
+        });
+        Property(e => e.Value, prop => {
+            prop.Column("Value");
+            prop.Type(NHibernateUtil.String);
+            prop.Length(256);
+            prop.NotNullable(true);
+        });
+    }
+}
