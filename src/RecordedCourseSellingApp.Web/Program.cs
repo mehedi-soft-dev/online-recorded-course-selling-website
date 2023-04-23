a96f662a4d1be186d677793bea6ac7cc8fa0bb92
Md. Mehedi Hasan <cse.mdmehedi@gmail.com>
2023-04-23 15:13:06 +0600

    add log config missing file

--- src/RecordedCourseSellingApp.Web/Program.cs
+++ src/RecordedCourseSellingApp.Web/Program.cs
@@ -18,28 +18,35 @@ builder.Logging.AddLog4Net();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+var log = LogManager.GetLogger(typeof(Program));
 
-ILog log = LogManager.GetLogger(typeof(Program));
-log.Info("Application Starting up..");
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var app = builder.Build();
+    log.Info("Application Starting up..");
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    log.Fatal($"Exception Message: {ex.Message}\nException: {ex}");
+}
