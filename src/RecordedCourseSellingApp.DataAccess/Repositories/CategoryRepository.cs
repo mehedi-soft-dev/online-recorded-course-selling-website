cbd7cb00c4412aa996f86f8717b0877d64bd943b
Md. Mehedi Hasan <cse.mdmehedi@gmail.com>
2023-04-25 22:55:46 +0600

    category - add repository

---
+++ src/RecordedCourseSellingApp.DataAccess/Repositories/CategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using NHibernate;
+using RecordedCourseSellingApp.DataAccess.Entities;
+namespace RecordedCourseSellingApp.DataAccess.Repositories;
+
+public class CategoryRepository : Repository<Category, Guid>, ICategoryRepository
+{
+    public CategoryRepository(ISession session) : base(session)
+    {
+
+    }
+}
