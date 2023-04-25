2beddd18f6be9723f7cb43e26b1c67f3d83b8e38
Md. Mehedi Hasan <cse.mdmehedi@gmail.com>
2023-04-25 22:52:01 +0600

    category - add entity

---
+++ src/RecordedCourseSellingApp.DataAccess/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordedCourseSellingApp.DataAccess.Entities;
+
+public class Category : IEntity<Guid>
+{
+    public virtual Guid Id { get; set; }
+
+    public virtual string Name { get; set; } = string.Empty;
+
+    public virtual string? Description { get; set; }
+}
