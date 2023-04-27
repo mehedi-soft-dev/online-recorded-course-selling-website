aa492a9a96718bce56db65379db1c2a90ba14efd
Md. Mehedi Hasan <cse.mdmehedi@gmail.com>
2023-04-27 19:23:40 +0600

    design course page

---
+++ src/RecordedCourseSellingApp.Web/Controllers/CourseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecordedCourseSellingApp.Web.Controllers;
+
+[Authorize]
+public class CourseController : Controller
+{
+    [AllowAnonymous]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
