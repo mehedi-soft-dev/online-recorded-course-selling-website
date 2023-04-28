9d272db340ed6dfba7e52c29683a842446d8e52c
Md. Mehedi Hasan <cse.mdmehedi@gmail.com>
2023-04-28 16:12:41 +0600

    Add cart item entity and mapping configuration

---
+++ src/RecordedCourseSellingApp.DataAccess/Entities/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using RecordedCourseSellingApp.DataAccess.Identity.Entities;
+
+namespace RecordedCourseSellingApp.DataAccess.Entities
+{
+    public class CartItem
+    {
+        public virtual Guid Id { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        public virtual Course Course { get; set; }
+
+        public virtual int Price { get; set; }
+    }
+}
